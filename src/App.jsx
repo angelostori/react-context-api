e69d253405e8f82da841ee3b1c2238351ccdf582
angelostori <angelostori13@gmail.com>
2025-11-18 12:51:08 +0100

    wrap app in BudgetContext provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useContext } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
 import Product from "./pages/Product";
+import { BudgetContext } from "./contexts/BudgetContext";
 
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="products" element={<Products />} />
-          <Route path="product/:id" element={<Product />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <BudgetContext.Provider value={"{budgetMode, setBudgetMod}"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="products" element={<Products />} />
+            <Route path="product/:id" element={<Product />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </BudgetContext.Provider>
   );
 }
